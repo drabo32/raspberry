0a0df4cc7274143e6ad6147ded53d971518471a5
drabo32 <drabo@free.fr>
2016-09-23 11:48:12 +0200

    Rajout fichier sauvegarde domoticz

---
+++ sauveDomoticz.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+date
+DATE=$((`/bin/date +'%Y%m%d%H%M'`))
+#find /media/sauveDomoticz/. -type f -mtime +5 -exec sudo rm -r {} \;
+find /media/sauveDomoticz/. -maxdepth 1 -type d -mtime +5 -exec sudo rm -r {} \;
+sudo mkdir /media/sauveDomoticz/domoticz_$DATE
+sudo cp -r ~/domoticz/ /media/sauveDomoticz/domoticz_$DATE/
+sudo tar cvf /media/sauveDomoticz/domoticz_$DATE.tar /media/sauveDomoticz/domoticz_$DATE/
+sudo rm -r  /media/sauveDomoticz/domoticz_$DATE/
+gzip /media/sauveDomoticz/domoticz_$DATE.tar
+date
