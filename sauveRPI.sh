994fc72c36587f17118814144f7ae37118725caf
drabo32 <drabo@free.fr>
2017-06-28 18:32:48 +0200

    Update sauveRPI.sh

--- sauveRPI.sh
+++ sauveRPI.sh
@@ -5,5 +5,6 @@ echo 'On efface les fichiers du ' $DATE_SEMAINE_DERNIERE
 rm /media/sauveRPI/*$DATE_SEMAINE_DERNIERE*
 DATE=$((`/bin/date +'%Y%m%d'`))
 sudo dd if=/dev/mmcblk0 | gzip -9 > /media/sauveRPI/raspberry-$DATE-mmcblk0.img.gz
-#sudo dd if=/dev/mmcblk0p2 | gzip -9 > /media/sauveRPI/raspberry-$DATE-mmcblk0p2.img.gz
+#Pour restaurer : gunzip ./raspberry-20130420-sdb.img.gz | sudo dd of=/dev/mmcblk0
+#Site où j'ai pris les infos : https://blog.nicolargo.com/2013/04/raspberry-pi-faire-un-backup-de-sa-carte-sd.html
 date
